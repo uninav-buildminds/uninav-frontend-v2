e10ea8dc622e96846fe5515abcd5b8ae27f3bd7f
hermes <hermeschigoziri@gmail.com>
2025-08-26 10:02:15 +0100

    feat: add useAuth hook for managing user authentication state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Menu, X } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
+import useAuth from "@/hooks/use-auth";
 
 const ChevronDownIcon = () => (
   <svg
@@ -17,6 +18,11 @@ const ChevronDownIcon = () => (
 
 const Header: React.FC = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const { user, refreshAuthState } = useAuth();
+
+  useEffect(() => {
+    refreshAuthState();
+  }, [refreshAuthState]);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -48,18 +54,20 @@ const Header: React.FC = () => {
 
         {/* Desktop Actions */}
         <div className="hidden md:flex items-center gap-3">
-          <a
-            href="#upload"
-            className="inline-flex items-center justify-center rounded-full px-4 py-2 text-sm font-medium text-white bg-brand"
-          >
-            Upload
-          </a>
-          <a
-            href="/auth/signin"
-            className="inline-flex items-center justify-center rounded-full border border-brand px-4 py-2 text-sm font-medium text-brand"
-          >
-            Sign In
-          </a>
+          {user && (
+						<a
+							href="#upload"
+							className="inline-flex items-center justify-center rounded-full px-4 py-2 text-sm font-medium text-white bg-brand">
+							Upload
+						</a>
+					)}
+					{!user && (
+						<a
+							href="/auth/signin"
+							className="inline-flex items-center justify-center rounded-full border border-brand px-4 py-2 text-sm font-medium text-brand">
+							Sign In
+						</a>
+					)}
         </div>
 
         {/* Mobile Menu Button */}
---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,28 @@
+import { UserProfile } from "@/lib/types/user.types";
+import { API_BASE_URL } from "@/lib/utils";
+import { useCallback, useEffect, useState } from "react";
+
+/**
+ * Custom hook to manage user authentication state and actions.
+ */
+export default function useAuth() {
+	const [user, setUser] = useState<UserProfile | null>(null);
+
+	const refreshAuthState = useCallback(async () => {
+		const response = await fetch(`${API_BASE_URL}/user/profile`, {
+			credentials: "include",
+		});
+		if (response.ok) {
+			const data = await response.json();
+			console.log(data.data);
+			setUser(data.data);
+		} else {
+			setUser(null);
+		}
+	}, []);
+
+	return {
+		refreshAuthState,
+		user,
+	};
+}
