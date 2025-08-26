df6a124f185ca65b92057080a3a27beaeb12738f
hermes <hermeschigoziri@gmail.com>
2025-08-26 10:01:28 +0100

    feat: add user types and approval status enum

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
