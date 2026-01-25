3ef2c08fdf84ecb119ad4c9fb079041c90bd48e2
hermes <hermeschigoziri@gmail.com>
2026-01-25 13:57:21 +0100

    update imports

--- src/components/dashboard/InstallPWA.tsx
+++ src/components/dashboard/InstallPWA.tsx
@@ -1,4 +1,5 @@
-import { AndroidIcon } from "hugeicons-react";
+import { AndroidIcon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
 import { useEffect, useState } from "react";
 
 interface InstallPWAProps {
@@ -59,7 +60,7 @@ const InstallPWA = (props: InstallPWAProps) => {
 				aria-label="Install UniNav"
 				title="Install UniNav"
 				className="w-full text-sm text-white bg-brand rounded-lg px-4 py-2 flex items-center justify-center gap-2 transition-colors">
-				<AndroidIcon size={16} />
+				<HugeiconsIcon icon={AndroidIcon} size={16} />
 				Install
 			</button>
 		);
@@ -71,7 +72,7 @@ const InstallPWA = (props: InstallPWAProps) => {
 			title="Install UniNav"
 			onClick={onClick}>
 			<div className="rounded-2xl transition-all duration-200 hover:scale-105 hover:bg-[#DCDFFE] h-10 w-10 grid place-items-center ">
-				<AndroidIcon size={18} className="text-gray-700" />
+				<HugeiconsIcon icon={AndroidIcon} size={16} className="text-gray-700" />
 			</div>
 			<span className="text-[11px] text-gray-700 text-wrap font-normal">
 				Install
